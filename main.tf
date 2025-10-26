3021b236003df78d7d3343856389d7e308d5fefa
Sridevi Nandipati <71425247+Sridevi6202@users.noreply.github.com>
2025-10-27 01:36:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "aws_launch_template" "web_server_as" {
   resource "aws_elb" "web_server_lb"{
      name = "web-server-lb"
      security_groups = [aws_security_group.web_server.id]
-     subnets = "subnet-01898abf1839791a1"
+     subnets = ["subnet-01898abf1839791a1", "subnet-0bcb03510267cfd40"]
      listener {
       instance_port     = 80
       instance_protocol = "http"
@@ -33,7 +33,7 @@ resource "aws_autoscaling_group" "web_server_asg" {
     desired_capacity     = 2
     health_check_type    = "EC2"
     load_balancers       = [aws_elb.web_server_lb.name]
-    availability_zones    = "us-east-1a" 
+    availability_zones    = ["us-east-1a", "us-east-1b"]
     launch_template {
         id      = aws_launch_template.web_server_as.id
         version = "$Latest"

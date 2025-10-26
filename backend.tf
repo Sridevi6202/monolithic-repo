4152cac34746a1e8da990c747ba69dd3ce64ebf3
Sridevi Nandipati <71425247+Sridevi6202@users.noreply.github.com>
2025-10-27 01:46:18 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
      region = "us-east-1"
-     bucket = "sridevi06.flm.devops.project.bucket"
+     bucket = "sridevi06.devsecops.project.bucket"
      key = "prod/terraform.tfstate"
   }
 }

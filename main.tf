02e42dd56fb2959cf9538f46bfe3f641675a2885
Sridevi Nandipati <71425247+Sridevi6202@users.noreply.github.com>
2025-10-27 01:41:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -33,12 +33,10 @@ resource "aws_autoscaling_group" "web_server_asg" {
     desired_capacity     = 2
     health_check_type    = "EC2"
     load_balancers       = [aws_elb.web_server_lb.name]
-    availability_zones    = ["us-east-1a", "us-east-1b"]
+    vpc_zone_identifier  = ["subnet-01898abf1839791a1", "subnet-0bcb03510267cfd40"]
     launch_template {
         id      = aws_launch_template.web_server_as.id
         version = "$Latest"
       }
-    
-    
   }
 
